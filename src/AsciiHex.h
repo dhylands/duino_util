639aad59226cd621ead8f9b55c0c369c21ed09c3
Dave Hylands <dhylands@gmail.com>
2024-11-10 18:14:28 -0800

    Fixed build actions

--- src/AsciiHex.h
+++ src/AsciiHex.h
@@ -32,5 +32,5 @@ using ByteBuffer = std::vector<uint8_t>;
 //!     @endcode
 //      `bytes[0] will contain 0x12 and bytes[1] will contain 0x34.
 //! @returns a uint8_t vector containing the bytes parsed from the string.
-ByteBuffer AsciiHexToBinary(const char* str  //!< [in] String to convert.
+ByteBuffer AsciiHexToBinary(char const* str  //!< [in] String to convert.
 );
